f2c48e86ed57dfc9e44b3a0f8651bf710c6656fd
Daniel R Gorgonha <rogergorgonha@gmail.com>
2025-05-06 23:56:15 -0300

    feat: started forge project

---
+++ bootcamp/week1/lesson2/playground/src/Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint256 private number;
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
